98d40d489dd6eec610f440d8521b6cd16aa320b1
Rob Rati <rrati@blackducksoftware.com>
2018-04-12 12:00:53 -0400

    Revert "Added test case for private registries"
    This reverts commit 00c669089e94039e47b9a2de21a2595e581abcdd.

--- pkg/docker/parseimage_test.go
+++ pkg/docker/parseimage_test.go
@@ -56,13 +56,6 @@ func TestParseImageIDString(t *testing.T) {
 			shouldPass:  true,
 		},
 		{
-			description: "valid format with private registry",
-			prefix:      "docker-pullable://",
-			name:        "docker-registry.default.svc:5000/def/ghi",
-			sha:         "cb4983d8399a59bb5ee6e68b6177d878966a8fe41abe18a45c3b1d8809f1d043",
-			shouldPass:  true,
-		},
-		{
 			description: "missing prefix",
 			prefix:      "",
 			name:        "abc/def",
